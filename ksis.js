6f1488f77e2a50d90c1638789a9f2c154af6e682
alafyte <nroussak0405@gmail.com>
2022-12-12 23:03:50 +0300

    ксис

--- ksis.js
+++ ksis.js
@@ -4,20 +4,20 @@ function calculate()
     var addr_splitted = input[0].value.split('.');
     var netmask_splitted = input[1].value.split('.');
 
-    document.writeln(`IP: ${addr_splitted.join('.')}<br>`);
-    document.writeln(`Mask: ${netmask_splitted.join('.')}<br>`);
+    document.writeln(`<text style="font-family:'Segoe UI'">IP: ${addr_splitted.join('.')}</text><br>`);
+    document.writeln(`<text style="font-family:'Segoe UI'">Mask: ${netmask_splitted.join('.')}</text><br>`);
 
-    document.writeln("NetworkID");
+    document.writeln(`<text style="font-family:'Segoe UI'">NetworkID</text>`);
     let networkid = [addr_splitted[0] & netmask_splitted[0], addr_splitted[1] & netmask_splitted[1], addr_splitted[2] & netmask_splitted[2],addr_splitted[3] & netmask_splitted[3],].join('.');
-    document.writeln(networkid + "<br>");
+    document.writeln(`<text style="font-family:'Segoe UI'">${networkid}</text><br>`);
 
-    document.writeln("HostID");
+    document.writeln(`<text style="font-family:'Segoe UI'">HostID</text>`);
     let hostid = [addr_splitted[0] & ~netmask_splitted[0], addr_splitted[1] & ~netmask_splitted[1], addr_splitted[2] & ~netmask_splitted[2],addr_splitted[3] & ~netmask_splitted[3],].join('.');
-    document.writeln(hostid + "<br>");
+    document.writeln(`<text style="font-family:'Segoe UI'">${hostid}</text><br>`);
 
     //оомагад no comments
-    document.writeln("Broadcast");
+    document.writeln(`<text style="font-family:'Segoe UI'">Broadcast</text>`);
     let broadcast = [addr_splitted[0] | (addr_splitted[0] & ~netmask_splitted[0] | (~netmask_splitted[0] & 0xFF)),addr_splitted[1] | ( addr_splitted[1] & ~netmask_splitted[1] | (~netmask_splitted[1] & 0xFF)), addr_splitted[2] | (addr_splitted[2] & ~netmask_splitted[2] | (~netmask_splitted[2] & 0xFF)), addr_splitted[3] | (addr_splitted[3] & ~netmask_splitted[3] | (~netmask_splitted[3] & 0xFF))].join('.');
 
-    document.writeln(broadcast);
+    document.writeln(`<text style="font-family:'Segoe UI'">${broadcast}</text>`);
 }
